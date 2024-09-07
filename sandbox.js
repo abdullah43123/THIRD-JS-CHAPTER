3274ba32e2a20cd35d4b56067727a9854b332df0
Abdullah <aabdullahaslam91@gmail.com>
2024-09-07 20:22:43 +0500

    THIRD CHAPTER

---
+++ sandbox.js
@@ -0,0 +1,15 @@
+var age = 20;
+alert("I am " + age + "  years old");
+
+var visit = 14;
+alert("You have visited this site " + visit + " times.");
+
+var birthyear = 2004;
+document.getElementById("demo").innerHTML = "My birth year is " + birthyear + "\n Data type of my declared variable is number";
+
+var visitorName = "ABDULLAH ASLAM";
+var productTitle = "SUBWRAP";
+var quantity = 50;
+
+console.log(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ FOOD Brand.");
+
